2d477e6845500c9c1ade5e0c9a5c20d9c382e661
yefimchuk <dmitriy.yefimchuk@gmail.com>
2022-06-27 01:18:12 +0300

    deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FeedbackModule } from './feedback/feedback.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Message } from "./entity/message.entity";
+import { Message } from './entity/message.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'feedback-db',
+      type: 'postgres',
+      host: 'ec2-52-212-228-71.eu-west-1.compute.amazonaws.com',
+      port: 5432,
+      username: 'dtmzfotfwojcds',
+      password:
+        'a6163b94d4c99bc5337dc61487008de2d3216442e0109908116ebb9a61a8c138',
+      database: 'd4tdmh2842pmkb',
       entities: [Message],
       synchronize: true,
     }),
