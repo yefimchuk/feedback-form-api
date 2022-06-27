986bf87feebce2c847178ee72a3d4a8dc47a5313
yefimchuk <dmitriy.yefimchuk@gmail.com>
2022-06-27 18:38:14 +0300

    try deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,11 @@ import { Message } from './entity/message.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'ec2-52-212-228-71.eu-west-1.compute.amazonaws.com',
+      host: 'localhost',
       port: 5432,
-      username: 'dtmzfotfwojcds',
-      password:
-        'a6163b94d4c99bc5337dc61487008de2d3216442e0109908116ebb9a61a8c138',
-      database: 'd4tdmh2842pmkb',
+      username: 'postgres',
+      password: '23984723',
+      database: 'feedback-db',
       entities: [Message],
       synchronize: true,
     }),
