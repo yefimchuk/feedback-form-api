362b014b0efb2495bb76f79a95b149cae5a57044
yefimchuk <dmitriy.yefimchuk@gmail.com>
2022-06-27 23:05:59 +0300

    update env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
 import { FeedbackModule } from './feedback/feedback.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Message } from './entity/message.entity';
+
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: 'postgres://utjsorhawubpdj:51a4b642beec0928e4d015903b91fb22f67e1d5878ad4f9eff45527150b9d436@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/d5tmu0ucsl5jv1',
+      url: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false },
       autoLoadEntities: true,
       synchronize: true,
